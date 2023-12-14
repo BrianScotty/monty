e2be1d2104cf47e49c5ba591f2c109a8fc65d529
BrianScotty <scotty.brianleong@gmail.com>
2023-12-14 11:41:05 -0800

    task 11

--- execute.c
+++ execute.c
@@ -12,22 +12,22 @@
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 instruction_t instructions[] = {
-        {"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
-        {"pop", f_pop},
-        {"swap", f_swap},
-        {"add", f_add},
-        {"nop", f_nop},
-        {"sub", f_sub},
-        {"div", f_div},
-        {"mul", f_mul},
-        {"mod", f_mod},
-        {"pchar", f_pchar},
-        {"pstr", f_pstr},
-        {"rotl", f_rotl},
-        {"rotr", f_rotr},
-        {"queue", f_queue},
-        {"stack", f_stack},
-        {NULL, NULL}
+	{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+	{"pop", f_pop},
+	{"swap", f_swap},
+	{"add", f_add},
+	{"nop", f_nop},
+	{"sub", f_sub},
+	{"div", f_div},
+	{"mul", f_mul},
+	{"mod", f_mod},
+	{"pchar", f_pchar},
+	{"pstr", f_pstr},
+	{"rotl", f_rotl},
+	{"rotr", f_rotr},
+	{"queue", f_queue},
+	{"stack", f_stack},
+	{NULL, NULL}
 };
 
 unsigned int i = 0;
@@ -36,7 +36,7 @@ char *op;
 op = strtok(content, " \n\t");
 
 if (op && op[0] == '#')
-return 0; /* Line is a comment */
+return (0); /* Line is a comment */
 
 bus.arg = strtok(NULL, " \n\t");
 
@@ -45,7 +45,7 @@ while (instructions[i].opcode && op)
 if (strcmp(op, instructions[i].opcode) == 0)
 {
 instructions[i].f(stack, counter);
-return 1; /* Line is not a comment */
+return (1); /* Line is not a comment */
 }
 i++;
 }
@@ -57,5 +57,5 @@ free(content);
 free_stack(*stack);
 exit(EXIT_FAILURE);
 }
-return 1; /* Line is not a comment */
+return (1); /* Line is not a comment */
 }
