eaba8c03c696dd3743e5744417d40c7a0ff28c29
BrianScotty <scotty.brianleong@gmail.com>
2023-12-14 12:17:53 -0800

    task 15

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - sets format of the data to stack
+ * @head: first node
+ * @counter: line number
+ * Return: void
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
