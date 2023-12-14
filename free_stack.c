08ac0bf692caad1d88bc740dde114ee398a47163
BrianScotty <scotty.brianleong@gmail.com>
2023-12-14 11:44:37 -0800

    task 11

--- free_stack.c
+++ free_stack.c
@@ -7,13 +7,13 @@
  */
 void free_stack(stack_t *head)
 {
-  stack_t *current, *next;
-  
-  current = head;
-  while (current != NULL)
-  {
-    next = current->next;
-    free(current);
-    current = next;
-  }
+	stack_t *current, *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
 }
